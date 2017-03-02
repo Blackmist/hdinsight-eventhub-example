01cc4d2f7074df1576a3a65874bdbb30beda0503
Larry Franks <larry.franks@microsoft.com>
2017-03-02 16:55:25 -0500

    Updates for HDInsight 3.5

--- TemperatureMonitor/src/main/java/com/microsoft/examples/bolts/ParserBolt.java
+++ TemperatureMonitor/src/main/java/com/microsoft/examples/bolts/ParserBolt.java
@@ -1,11 +1,11 @@
 package com.microsoft.examples;
 
-import backtype.storm.topology.base.BaseBasicBolt;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Values;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
 
 import org.json.JSONObject;
 
